6606a4cef1bb5163bc4fca6f697ef391dc1f7414
Christian Torbica <uplink.chris@gmail.com>
2020-11-24 11:39:39 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Net.Http;
+using System.IO;
+
+using System.Text;
+
+namespace GWWikiDailyReminder
+{
+    class Program
+    {
+
+        public readonly static string siteUrl = "https://wiki.guildwars.com/wiki/Daily_activities";
+
+
+        static async void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(siteUrl);
+
+
+        }
+    }
+}

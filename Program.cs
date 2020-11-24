cb277f8e0950861d39fb90e6948f03ba35f5ac46
Christian Torbica <uplink.chris@gmail.com>
2020-11-24 11:49:10 +0100

    Gets page and prints H1

--- Program.cs
+++ Program.cs
@@ -3,23 +3,24 @@ using System.Linq;
 using System.Threading;
 using System.Net.Http;
 using System.IO;
-
 using System.Text;
+using HtmlAgilityPack;
 
 namespace GWWikiDailyReminder
 {
     class Program
     {
-
         public readonly static string siteUrl = "https://wiki.guildwars.com/wiki/Daily_activities";
 
-
-        static async void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
             var httpClient = new HttpClient();
             var html = await httpClient.GetStringAsync(siteUrl);
 
+            var document = new HtmlDocument();
+            document.LoadHtml(html);
 
+            Console.WriteLine("Title of the page: " + document.DocumentNode.Descendants("h1").FirstOrDefault().InnerText);
         }
     }
 }

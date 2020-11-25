20e2fea1d06190e7107434849c4910380167dfae
Christian Torbica <uplink.chris@gmail.com>
2020-11-25 10:44:48 +0100

    Refactor of DailyHTMLTable

--- Program.cs
+++ Program.cs
@@ -11,8 +11,9 @@ namespace GWWikiDailyReminder
 {
     class Program
     {
-        public readonly static string siteUrl = "https://wiki.guildwars.com/wiki/Daily_activities";
-        public readonly static string wikiUrl = "https://wiki.guildwars.com/wiki/";
+        public readonly static string dailiesSiteURL = "https://wiki.guildwars.com/wiki/Daily_activities";
+        public readonly static string weekliesSiteURL = "https://wiki.guildwars.com/wiki/Weekly_activities";
+        public readonly static string wikiURL = "https://wiki.guildwars.com/wiki/";
 
         public static void SendDailyMail(string htmlMessage, Dictionary<string, string> parameters)
         {
@@ -55,6 +56,18 @@ namespace GWWikiDailyReminder
             }
         }
 
+        public static string GetDailyHTMLTable(HtmlNode table)
+        {
+            var sb = new StringBuilder("<table style=\"border: 1px solid black\"><thead>");
+            sb.AppendLine(table.SelectSingleNode(".//tr[.//th[contains(text(), 'Date')]]").InnerHtml.ToString());
+            sb.AppendLine("</thead><tbody>");
+            sb.AppendLine($"<tr>{table.SelectSingleNode($".//tr[.//td[contains(text(), '{DateTime.Now.ToString("d MMMM yyyy")}')]]").InnerHtml.ToString()}</tr>");
+            sb.AppendLine($"<tr>{table.SelectSingleNode($".//tr[.//td[contains(text(), '{DateTime.Now.AddHours(24).ToString("d MMMM yyyy")}')]]").InnerHtml.ToString()}</tr>");
+            sb.AppendLine("</tbody></table>");
+            sb.Replace("/wiki/", wikiURL);
+            return sb.ToString();
+        }
+
         static async System.Threading.Tasks.Task Main(string[] args)
         {
             if(args.Length == 0)
@@ -63,22 +76,14 @@ namespace GWWikiDailyReminder
             }
 
             var httpClient = new HttpClient();
-            var html = await httpClient.GetStringAsync(siteUrl);
+            var html = await httpClient.GetStringAsync(dailiesSiteURL);
 
             var document = new HtmlDocument();
             document.LoadHtml(html);
 
             var table = document.DocumentNode.SelectSingleNode("//table");
 
-            var sb = new StringBuilder("<table style=\"border: 1px solid black\"><thead>");
-            sb.AppendLine(table.SelectSingleNode(".//tr[.//th[contains(text(), 'Date')]]").InnerHtml.ToString());
-            sb.AppendLine("</thead><tbody>");
-            sb.AppendLine($"<tr>{table.SelectSingleNode($".//tr[.//td[contains(text(), '{DateTime.Now.ToString("d MMMM yyyy")}')]]").InnerHtml.ToString()}</tr>");
-            sb.AppendLine($"<tr>{table.SelectSingleNode($".//tr[.//td[contains(text(), '{DateTime.Now.AddHours(24).ToString("d MMMM yyyy")}')]]").InnerHtml.ToString()}</tr>");
-            sb.AppendLine("</tbody></table>");
-            sb.Replace("/wiki/", wikiUrl);
-
-            SendDailyMail(sb.ToString(), ReadParams(args[0]));
+            SendDailyMail(GetDailyHTMLTable(table), ReadParams(args[0]));
         }
     }
 }
